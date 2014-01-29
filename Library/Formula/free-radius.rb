71459594b97bb126a5eaccb280cf968d6db8db8c
Clifford Heath <clifford.heath@gmail.com>
2014-01-29 16:14:12 +1100

    Added freeRADIUS version 3

---
+++ Library/Formula/free-radius.rb
@@ -0,0 +1,44 @@
+require 'formula'
+
+class FreeRadius < Formula
+  homepage 'http://freeradius.org/'
+  url 'ftp://ftp.freeradius.org/pub/freeradius/freeradius-server-3.0.1.tar.bz2'
+  sha1 '3115f18fbe550460617f5a3402017e9b675a8726'
+
+  # Requires newer autotools on all platforms
+  depends_on 'autoconf' => :build
+  depends_on 'automake' => :build
+  depends_on 'libtool' => :build
+
+  # libtool is glibtool on OS X
+  # def patches; DATA end
+
+  def install
+    bin.install # "ip_relay.pl" => "ip_relay"
+    ENV.deparallelize
+
+    system "autoreconf", "-fvi"
+    system "./configure", "--prefix=#{prefix}",
+                          "--with-system-libtool",
+                          "--with-system-libltdl"
+    system "make"
+    system "make install"
+  end
+end
+
+__END__
+diff --git a/configure.in b/configure.in
+index 62b0de8..97e0243 100644
+--- a/configure.in
++++ b/configure.in
+@@ -101,7 +101,7 @@ AC_SUBST(LTDL_SUBDIRS)
+ dnl use system-wide libtool, if it exists
+ AC_ARG_WITH(system-libtool,
+ [  --with-system-libtool   Use the libtool installed in your system (default=use our own)],
+-[ AC_PATH_PROG(LIBTOOL, libtool,,$PATH:/usr/local/bin) AC_LIBTOOL_DLOPEN
++[ AC_PATH_PROG(LIBTOOL, glibtool,,$PATH:/usr/local/bin) AC_LIBTOOL_DLOPEN
+  AC_PROG_LIBTOOL],
+ [
+   LIBTOOL="`pwd`/libtool"
+  end
+end
